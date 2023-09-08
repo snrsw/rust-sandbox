5b863e8b4c6c899ae4593b996448e19f52d50197
snarisawa <shota.narisawa@jdsc.ai>
2023-09-08 22:44:31 +0900

    Update: main: to add some samples

--- hello/hello_rust/src/main.rs
+++ hello/hello_rust/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     println!("spaces: {}", spaces);
     let spaces = spaces.len();
     println!("spaces: {}", spaces);
+
+    let (x, y): (i32, i64) = (5, 6);
+    println!("x: {}, y: {}", x, y);
+
+    let arr: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("arr: {:?}", arr);
+    println!("arr[0]: {:?}", arr[0]);
+
+    let slice: &[i32] = &arr;
+    println!("slice: {:?}", slice);
 }
