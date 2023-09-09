11eccfcfa3c95c1c8a9d167c546d4106bd745ae5
snrsw <ale.nnn.r@gmail.com>
2023-09-09 16:55:19 +0900

    Update: main: to add some samples

--- quick-study-2/hello_rust/src/main.rs
+++ quick-study-2/hello_rust/src/main.rs
@@ -1,7 +1,37 @@
+fn foo() -> String {
+    let s = String::from("Hello");
+    s
+}
+
+fn append(src: &Vec<i32>, dst: &mut Vec<i32>) {
+    for &x in src {
+        dst.push(x);
+    }
+}
+
 fn main() {
     let x: String;
     x = String::from("Hello, world!");
     println!("{}", x);
+
     let y = x;
     println!("{}", y);
+
+    let z = y.clone();
+    println!("{}", z);
+    println!("{}", y);
+
+    let hoge = foo();
+    println!("{}", hoge);
+
+    let mut v = vec![1, 2, 3];
+    append(&v.clone(), &mut v);
+    println!("{:?}", v);
+
+    let mut x = 5;
+    println!("{}", x);
+    let y = &mut x;
+    *y += 1;
+    println!("{}", y);
+    println!("{}", x);
 }
